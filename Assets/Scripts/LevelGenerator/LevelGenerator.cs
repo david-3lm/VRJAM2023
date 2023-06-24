ed048caa540fdddb0160a885afd3a38f2d7032e8
Rogarsu2014 <rogarsu2014@gmail.com>
2023-06-24 18:55:30 +0200

    Loopty loop
    En mi escena hay un terreno que va loopeando (como si fuera un endless runner vaya)
    En principio debería funcionar con cualquier modelo 3d así que los que vayan a hacer el mapa de verdad tendrán que tocarla y comprobarlo.

---
+++ Assets/Scripts/LevelGenerator/LevelGenerator.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    [SerializeField] private GameObject terreno; //El modelo 3d del terreno (tiene que tener un rigidbody sin gravedad ni kinetico)
+    private Vector3 terrenoSize; //Lo sacamos a partir del modelo 3d
+    [SerializeField] private List<GameObject> TerrenoPool; //La pool de objetos para el terreno
+    [SerializeField] private int poolSize; //El tamaño que ponemos nosotros que va a tener la pool
+    [SerializeField] private int velocidadTerreno; //La velocidad a la que va el terreno (la velocidad de la nave)
+    private GameObject prevTerreno; //Una referencia al un terreno cualquiera (para usarlo sin tener que inicializar un objeto)
+    [SerializeField] private List<GameObject> terrenosActivos; //La referencia a los terrenos que estén activos
+
+    public GameObject primerTerreno; //el ultimo terreno que esta (para poder poner el siguiente)
+    private int terrenosIniciales = 3; //Cuantos terrenos va a haber a la vez en pantalla)
+    private float distanciaUltimoTerrenoPlayer; //A que distancia debe desaparecer el terreno
+    void Start()
+    {
+        //Instanciar la object Pool
+        TerrenoPool = new List<GameObject>();
+        GameObject tmp;
+        for (int i = 0; i < poolSize; i++)
+        {
+            tmp = Instantiate(terreno);
+            tmp.SetActive(false);
+            TerrenoPool.Add(tmp);
+        }
+        //Pillar el tamaño de los objetos de la pool
+        terrenoSize= GetTerreno().GetComponent<MeshRenderer>().bounds.size;
+
+        //Referencia a los objetos de la pool que estén activos
+        terrenosActivos = new List<GameObject>();
+
+        //Crear los terrenos iniciales
+        float mitad = (terrenosIniciales / 2);
+
+        for (int i = 0; i < terrenosIniciales; i++)//Inicializar los terrenos que salgan al principio
+        {
+            prevTerreno = GetTerreno();
+            prevTerreno.transform.position = new Vector3(terrenoSize.x*(i-mitad),0,0);//Supongo que terrenos iniciales es impar
+            prevTerreno.GetComponent<Rigidbody>().velocity = new Vector3(-velocidadTerreno, 0, 0);
+            prevTerreno.SetActive(true);
+            terrenosActivos.Add(prevTerreno);
+        }
+        //El último terreno es el primero(si lo contamos como desde atras)
+        primerTerreno = prevTerreno;
+        //Calculamos esta distancia (va a ser siempre un valor fijo a partir de este punto)
+        distanciaUltimoTerrenoPlayer = terrenoSize.x * mitad;
+    }
+    
+    void Update()
+    {
+        if(terrenosActivos[0].transform.position.x < -distanciaUltimoTerrenoPlayer)//Si el terreno se salio se desactiva y vuelve a la pool)
+        {
+            terrenosActivos[0].SetActive(false);
+            terrenosActivos.RemoveAt(0);
+            SpawnTerreno(); //Sacamos uno nuevo en el lugar del que ha desaparecido
+        }
+    }
+
+    public GameObject GetTerreno() //Para pillar objetos de la pool (sacado de la documentación de Unity)
+    {
+        for (int i = 0; i < poolSize; i++)
+        {
+            if (!TerrenoPool[i].activeInHierarchy)
+            {
+                return TerrenoPool[i];
+            }
+        }
+        return null;
+    }
+
+    private void SpawnTerreno()//Sacar un terreno al principio del todo
+    {
+        prevTerreno = GetTerreno();
+        prevTerreno.transform.position = new Vector3(primerTerreno.transform.position.x + terrenoSize.x, 0, 0);
+        prevTerreno.GetComponent<Rigidbody>().velocity = new Vector3(-velocidadTerreno, 0, 0);
+        prevTerreno.SetActive(true);
+        terrenosActivos.Add(prevTerreno);
+        primerTerreno = prevTerreno;
+    }
+}
